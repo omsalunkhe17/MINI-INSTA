153e18cbf58d813b0db01a6f54984cb6b56b3919
Om Salunkhe <omsalunkhe1441@gmail.com>
2026-03-30 04:40:52 +0530

    fixed port issue for render deployment

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const app = require('./src/app');
-const connectToDb = require('./src/config/database')
-
+const connectToDb = require('./src/config/database');
 
 connectToDb();
 
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
